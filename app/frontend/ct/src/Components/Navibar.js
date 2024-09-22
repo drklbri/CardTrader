999286e3a04d44826511d845c39598cbfc8db5ec
drklbri <enotfrt@gmail.com>
2024-09-22 14:17:03 +0300

    updated auth

--- app/frontend/ct/src/Components/Navibar.js
+++ app/frontend/ct/src/Components/Navibar.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
-import './Navibar.css'; // Подключаем стили для темного дизайна
+import React, { useState, useContext } from 'react';
+import './Navibar.css';
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from './AuthContext';
 
-const Navigation = () => {
+
+const Navibar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
+    const { isAuthenticated, logout } = useContext(AuthContext); // Доступ к авторизации и функции logout
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
     const closeMenu = () => {
-        // Закрываем меню, только если оно открыто
         if (menuOpen) {
             setMenuOpen(false);
         }
+    };
+
+    const handleNavigation = (path) => {
+        navigate(path);
+        closeMenu();
+    };
+
+    const handleLogout = () => {
+        logout(); // Вызываем функцию logout из контекста
+        handleNavigation('/auth'); // Перенаправляем на страницу авторизации
     };
 
     return (
@@ -23,13 +37,22 @@ const Navigation = () => {
             </div>
             {menuOpen && (
                 <div className="menu" onMouseLeave={closeMenu}>
-                    <button className="menu-item">Главная</button>
-                    <button className="menu-item">Личный кабинет</button>
-                    <button className="menu-item">Выйти</button>
+                    <button className="menu-item" onClick={() => handleNavigation('/main')}>Главная</button>
+                    {isAuthenticated ? (
+                        <>
+                            <button className="menu-item" onClick={() => handleNavigation('/cabinet')}>Личный кабинет</button>
+                            <button className="menu-item" onClick={handleLogout}>Выйти</button> {/* Добавлена кнопка выхода */}
+                        </>
+                    ) : (
+                        <>
+                            <button className="menu-item" onClick={() => handleNavigation('/auth')}>Авторизация</button>
+                            <button className="menu-item" onClick={() => handleNavigation('/register')}>Регистрация</button>
+                        </>
+                    )}
                 </div>
             )}
         </div>
     );
 };
 
-export default Navigation;
+export default Navibar;
